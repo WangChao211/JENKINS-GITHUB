158daed1af4f3dc46980d70cde1aeee20a2d86c1
WangChao211 <565276012@qq.com>
2018-01-15 17:40:09 +0800

    第一次提交jenkinsgit

---
+++ src/main/java/cn/wang/jenkins/github/Index.java
@@ -0,0 +1,14 @@
+package cn.wang.jenkins.github;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Index {
+	@RequestMapping(value="index")
+	@ResponseBody
+	public String toIndex(){
+		return "hello,jenkins for github!!";
+	}
+}
---
+++ src/main/java/cn/wang/jenkins/github/JenkinsGitApplication.java
@@ -0,0 +1,12 @@
+package cn.wang.jenkins.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGitApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsGitApplication.class, args);
+	}
+	
+}
